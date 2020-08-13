5565c5d09c59cd50ade262b045a4338958d79a90
Michael Solwold <msolwold35@gmail.com>
2020-08-12 17:16:03 -0700

    Add Full Meal Type

--- app/src/models/Home/types.ts
+++ app/src/models/Home/types.ts
@@ -1,10 +1,22 @@
+import { DiabetesCalculator } from "../Shared/enumerations"
+
+export type MealInfo = {
+    mealDate: Date,
+    enteredDate: Date,
+    mealType: DiabetesCalculator.MealType | -1,
+    mealName: string,
+    description?: string
+}
+
 export type BGMealInfo = {
     currentBG: number,
+    targetBG: number,
     correction: boolean,
     correctionUnits?: number,
 }
 
 export type CarbMealInfo = {
     mealCarbs: number,
+    carbsPerUnit: number,
     insulinDose: number,
 }
--- app/src/models/Shared/types.ts
+++ app/src/models/Shared/types.ts
@@ -1,7 +1,16 @@
+import { MealInfo, BGMealInfo, CarbMealInfo } from "../Home/types";
+
 export type ListItem = {
 	key: number;
 	label: string;
 	value: string;
 };
+
+export type Meal = {
+	mealInfo: MealInfo | undefined,
+	bgMealInfo: BGMealInfo | undefined,
+	carbMealInfo: CarbMealInfo | undefined,
+	items?: Array<any>, // TODO: Create Item Type
+}
 
 
